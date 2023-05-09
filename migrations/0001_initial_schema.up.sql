4fd0c62aafcf80adf9ef3183a13a293818e8ef29
jillo-abdullahi <jillo.abdulla@gmail.com>
2023-05-09 14:12:02 +0300

    update db schema and corresponding routes

--- migrations/0001_initial_schema.up.sql
+++ migrations/0001_initial_schema.up.sql
@@ -2,13 +2,26 @@ CREATE TABLE supply_chain_items (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     color VARCHAR(255),
-    price DECIMAL(10, 2)
+    price NUMERIC,
+    skuCode UUID UNIQUE,
+    description TEXT,
+    createdOn TIMESTAMP,
+    city VARCHAR(255),
+    postalCode VARCHAR(20),
+    address TEXT,
+    quantity INTEGER,
+    shelfLife INTERVAL,
+    safetyStock INTEGER,
+    country VARCHAR(255)
 );
 
 CREATE TABLE item_events (
     id SERIAL PRIMARY KEY,
-    supply_chain_item_id INTEGER REFERENCES supply_chain_items (id),
+    item_id INTEGER REFERENCES supply_chain_items_v2(id),
+    eventTimestamp TIMESTAMP,
+    eventType ENUM('Shipment', 'Receipt', 'Transfer'),
+    eventStatus ENUM('Pending', 'Cancelled', 'Completed'),
     location VARCHAR(255),
     custodian VARCHAR(255),
-    timestamp TIMESTAMP
+    notes TEXT
 );
--- routes.js
+++ routes.js
@@ -1,37 +1,153 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const pool = require("./database");
 
 // Create a new supply chain item
 router.post("/items", async (req, res) => {
-  const { name, color, price } = req.body;
-  const result = await pool.query(
-    "INSERT INTO supply_chain_items (name, color, price) VALUES ($1, $2, $3) RETURNING *",
-    [name, color, price]
-  );
-  res.status(201).json(result.rows[0]);
+  const {
+    name,
+    color,
+    price,
+    skuCode,
+    description,
+    createdOn,
+    city,
+    postalCode,
+    address,
+    quantity,
+    shelfLife,
+    safetyStock,
+    country,
+  } = req.body;
+  const client = await pool.connect();
+
+  try {
+    const result = await client.query(
+      `
+      INSERT INTO supply_chain_items_v2 (name, color, price, skuCode, description, createdOn, city, postalCode, address, quantity, shelfLife, safetyStock, country)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+      RETURNING *;
+    `,
+      [
+        name,
+        color,
+        price,
+        skuCode,
+        description,
+        createdOn,
+        city,
+        postalCode,
+        address,
+        quantity,
+        shelfLife,
+        safetyStock,
+        country,
+      ]
+    );
+
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while creating the item" });
+  } finally {
+    client.release();
+  }
 });
 
 // Update supply chain item reference data
-router.put("/items/:id", async (req, res) => {
-  const id = parseInt(req.params.id);
-  const { name, color, price } = req.body;
-  const result = await pool.query(
-    "UPDATE supply_chain_items SET name=$1, color=$2, price=$3 WHERE id=$4 RETURNING *",
-    [name, color, price, id]
-  );
-  res.status(200).json(result.rows[0]);
+router.put("/items/:item_id", async (req, res) => {
+  const item_id = parseInt(req.params.item_id, 10);
+  const {
+    name,
+    color,
+    price,
+    skuCode,
+    description,
+    createdOn,
+    city,
+    postalCode,
+    address,
+    quantity,
+    shelfLife,
+    safetyStock,
+    country,
+  } = req.body;
+  const client = await pool.connect();
+
+  try {
+    const result = await client.query(
+      `
+      UPDATE supply_chain_items_v2
+      SET name = $1, color = $2, price = $3, skuCode = $4, description = $5, createdOn = $6, city = $7, postalCode = $8, address = $9, quantity = $10, shelfLife = $11, safetyStock = $12, country = $13
+      WHERE id = $14
+      RETURNING *;
+    `,
+      [
+        name,
+        color,
+        price,
+        skuCode,
+        description,
+        createdOn,
+        city,
+        postalCode,
+        address,
+        quantity,
+        shelfLife,
+        safetyStock,
+        country,
+        item_id,
+      ]
+    );
+
+    if (result.rows.length > 0) {
+      res.status(200).json(result.rows[0]);
+    } else {
+      res.status(404).json({ error: "Item not found" });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while updating the item" });
+  } finally {
+    client.release();
+  }
 });
 
-// Add new events associated with an item
-router.post("/items/:id/events", async (req, res) => {
-  const supply_chain_item_id = parseInt(req.params.id);
-  const { location, custodian, timestamp } = req.body;
-  const result = await pool.query(
-    "INSERT INTO item_events (supply_chain_item_id, location, custodian, timestamp) VALUES ($1, $2, $3, $4) RETURNING *",
-    [supply_chain_item_id, location, custodian, timestamp]
-  );
-  res.status(201).json(result.rows[0]);
+// add new events associated with an item
+router.post("/items/:item_id/events", async (req, res) => {
+  const item_id = parseInt(req.params.item_id, 10);
+  const { eventTimestamp, eventType, eventStatus, location, custodian, notes } =
+    req.body;
+  const client = await pool.connect();
+
+  try {
+    const result = await client.query(
+      `
+      INSERT INTO item_events_v2 (item_id, eventTimestamp, eventType, eventStatus, location, custodian, notes)
+      VALUES ($1, $2, $3, $4, $5, $6, $7)
+      RETURNING *;
+    `,
+      [
+        item_id,
+        eventTimestamp,
+        eventType,
+        eventStatus,
+        location,
+        custodian,
+        notes,
+      ]
+    );
+
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while creating the event" });
+  } finally {
+    client.release();
+  }
 });
 
 // Query all events of an item
@@ -53,6 +169,5 @@ router.get("/items/:id/events/last", async (req, res) => {
   );
   res.status(200).json(result.rows[0]);
 });
-
 
 module.exports = router;
